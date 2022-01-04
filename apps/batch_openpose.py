61d16baf08536db2d11edb5333c8b71bd1bea1af
Jakub Tustanowski <jakub.tustanowski@gmail.com>
2022-01-04 20:30:31 +0100

    Create both folders correctly in Batch OpenPose

--- apps/batch_openpose.py
+++ apps/batch_openpose.py
@@ -17,7 +17,8 @@ input_path = args.input_root
 out_path_json = args.out_path_json
 out_path_image = args.out_path_image
 
-os.makedirs(out_json_path, exist_ok=True)
+os.makedirs(out_path_json, exist_ok=True)
+os.makedirs(out_path_image, exist_ok=True)
 
 cmd = "cd {0}; ./build/examples/openpose/openpose.bin --image_dir {1} --write_json {2} --render_pose 2 --face --face_render 2 --hand --hand_render 2  --display 0 --write_images {3}".format(op_dir, input_path, out_path_json, out_path_image)
 print(cmd)
